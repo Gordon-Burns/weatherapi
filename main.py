e7e0d61873bc9fee00581207580b700a5c6f88f9
Gordon Burns <gordonburns@live.co.uk>
2023-05-27 13:26:20 +0100

    Added ability to search just by station or station for whole year

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import pandas as pd
 app = Flask(__name__)
 
 stations = df = pd.read_csv("data_small/stations.txt", skiprows=17)
-stations = stations[["STAID","STANAME                                 "]]
+stations = stations[["STAID", "STANAME                                 "]]
+
+
 @app.route("/")
 def home():
     return render_template("home.html", data=stations.to_html())
@@ -18,6 +20,23 @@ def about(station, date):
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filepath = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filepath, skiprows=20, on_bad_lines='skip', parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def all_data_single_year(station, year):
+    filepath = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filepath, skiprows=20, on_bad_lines='skip')
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
 
 
 if __name__ == "__main__":
